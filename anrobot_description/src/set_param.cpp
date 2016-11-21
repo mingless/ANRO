b8ec8b8370d44dc568dd124817547ca342be6e12
Mateusz Dziwulski <mdziwuls@pantera.robotyka.ia.pw.edu.pl>
2016-11-21 11:17:24 +0100

    Added joint_states position following

--- anrobot_description/src/set_param.cpp
+++ anrobot_description/src/set_param.cpp
@@ -3,69 +3,59 @@
 #include <sensor_msgs/JointState.h>
 #include "anrobot_description/FwdKinematics.h"
 
-int main(int argc, char** argv)
+typedef struct kinematicsManagmentStruct
 {
-    ros::init(argc, argv, "set_param_client");
-    if(argc != 5)
-    {
-        ROS_ERROR("Four params required: theta1 theta2 d3 use_kdl");
-        return 1;
-    }
+    bool use_kdl;
+    ros::Publisher *marker_pub;
+    ros::ServiceClient *kdl_client;
+    ros::ServiceClient *simple_client;
+} KinStruct;
 
-    ros::NodeHandle n;
-    ros::Rate r(1);
-
-    ros::Publisher marker_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 1);
-    ros::Publisher joint_states = n.advertise<sensor_msgs::JointState>("set_joint_states", 1);
-
-    ros::ServiceClient kdl_client = n.serviceClient<anrobot_description::FwdKinematics>("kdl_fwd");
-    ros::ServiceClient simple_client = n.serviceClient<anrobot_description::FwdKinematics>("simple_fwd");
-
+void joint_states_cb(const sensor_msgs::JointStateConstPtr &msg, KinStruct k)
+{
     anrobot_description::FwdKinematics srv;
-    srv.request.theta1 = atoll(argv[1]);
-    srv.request.theta2 = atoll(argv[2]);
-    srv.request.d3 = atoll(argv[3]);
-    srv.request.use_kdl = atoll(argv[4]);
+    srv.request.theta1 = msg->position[0];
+    srv.request.theta2 = msg->position[1];
+    srv.request.d3 = msg->position[2];
+    srv.request.use_kdl = k.use_kdl;
 
-    while (ros::ok())
-    {
-        visualization_msgs::Marker marker;
-        marker.header.frame_id = "/base_link";
-        marker.header.stamp = ros::Time::now();
+    visualization_msgs::Marker marker;
+    marker.header.frame_id = "/base_link";
+    marker.header.stamp = ros::Time::now();
 
-        marker.ns = "end_pose";
-        marker.id = 0;
-        marker.type = visualization_msgs::Marker::CUBE;
-        marker.action = visualization_msgs::Marker::ADD;
+    marker.ns = "end_pose";
+    marker.id = 0;
+    marker.type = visualization_msgs::Marker::CUBE;
+    marker.action = visualization_msgs::Marker::ADD;
 
-        marker.scale.x = 1.0;
-        marker.scale.y = 1.0;
-        marker.scale.z = 1.0;
+    marker.scale.x = 0.1;
+    marker.scale.y = 0.1;
+    marker.scale.z = 0.1;
 
-        // Set the color -- be sure to set alpha to something non-zero!
-        marker.color.r = 0.0f;
-        marker.color.g = 1.0f;
-        marker.color.b = 0.0f;
-        marker.color.a = 1.0;
+    // Set the color -- be sure to set alpha to something non-zero!
+    marker.color.r = 0.0f;
+    marker.color.g = 1.0f;
+    marker.color.b = 0.0f;
+    marker.color.a = 1.0;
 
-        marker.lifetime = ros::Duration();
+    marker.lifetime = ros::Duration();
 
         if(srv.request.use_kdl)
         {
-            if(kdl_client.call(srv))
+            if(k.kdl_client->call(srv))
             {
                 marker.pose = srv.response.end_pose;
             }
         }
         else
         {
-            if(simple_client.call(srv))
+            if(k.simple_client->call(srv))
             {
                 marker.pose = srv.response.end_pose;
             }
         }
 
-        while (marker_pub.getNumSubscribers() < 1)
+        /*while (k.marker_pub->getNumSubscribers() < 1)
         {
             if (!ros::ok())
             {
@@ -74,12 +64,36 @@ int main(int argc, char** argv)
 
             ROS_WARN_ONCE("Please create a subscriber to the marker");
             sleep(1);
-        }
+        }*/
 
-        marker_pub.publish(marker);
+        k.marker_pub->publish(marker);
+}
 
-        r.sleep();
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "set_param_client");
+    if(argc != 2)
+    {
+        ROS_ERROR_STREAM("Usage:  rosrun anrobot_description set_param arg" << "\n"
+		         << "arg:  0 for simple_fwd, 1 for kdl_fwd");
+        return 1;
     }
+
+    ros::NodeHandle n;
+
+    ros::Publisher marker_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 1);
+    ros::ServiceClient kdl_client = n.serviceClient<anrobot_description::FwdKinematics>("kdl_fwd");
+    ros::ServiceClient simple_client = n.serviceClient<anrobot_description::FwdKinematics>("simple_fwd");
+
+    KinStruct k;
+    k.use_kdl = atoi(argv[1]);
+    k.marker_pub = &marker_pub;
+    k.kdl_client = &kdl_client;
+    k.simple_client = &simple_client;
+    
+    ros::Subscriber get_joint_states = n.subscribe<sensor_msgs::JointState>("joint_states", 100, boost::bind(joint_states_cb, _1, k));
+    
+    ros::spin();
 
     return 0;
 }
