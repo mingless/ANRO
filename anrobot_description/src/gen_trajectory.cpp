31333d36822e2eaf95a0568f2394a76903e397a3
mingless <mateusz.dziwulski@gmail.com>
2016-12-05 07:44:23 +0100

    Fixed the introduced nonlinearity & some minor fixes

--- anrobot_description/src/gen_trajectory.cpp
+++ anrobot_description/src/gen_trajectory.cpp
@@ -9,20 +9,21 @@ class Trajectory
 {
     private:
         int time;
-        //a - current/starting points, b - target points
-        double a1, a2, a3, b1, b2, b3,
-               lin_delta_1, lin_delta_2, lin_delta_3;
-        sensor_msgs::JointState state;
-        ros::Publisher* pub_ptr;
+        sensor_msgs::JointState initial, target;
+        double lin_delta_0, lin_delta_1, lin_delta_2;
         void init();
 
     public:
         int msg_amount;
         int inc; //number of passed increments in current timer cycle
         Trajectory(ros::Publisher* pub);
+        ros::Publisher* pub_ptr;
+        sensor_msgs::JointState current;
 
-        void init_lin(double b1, double b2, double b3);
+        bool compare_target(sensor_msgs::JointStateConstPtr input);
+        void init_lin(sensor_msgs::JointStateConstPtr msg);
         void next_step_lin(const ros::TimerEvent& event);
+        void set_target(sensor_msgs::JointStateConstPtr target);
 
 };
 
@@ -31,19 +32,20 @@ void Trajectory::init()
     time = 10;
     msg_amount = 200;
     inc = 0;
-    state.position.push_back(0);
-    state.position.push_back(0);
-    state.position.push_back(1.65);
-    state.velocity.push_back(0);
-    state.velocity.push_back(0);
-    state.velocity.push_back(0);
-    state.effort.push_back(0);
-    state.effort.push_back(0);
-    state.effort.push_back(0);
-    state.name.push_back("joint1");
-    state.name.push_back("joint2");
-    state.name.push_back("joint3");
-    a1 = state.position[0]; a2 = state.position[1]; a3 = state.position[2];
+    current.position.push_back(0);
+    current.position.push_back(0);
+    current.position.push_back(1.65);
+    current.velocity.push_back(0);
+    current.velocity.push_back(0);
+    current.velocity.push_back(0);
+    current.effort.push_back(0);
+    current.effort.push_back(0);
+    current.effort.push_back(0);
+    current.name.push_back("joint1");
+    current.name.push_back("joint2");
+    current.name.push_back("joint3");
+    initial = current;
+    target = current;
 }
 
 Trajectory::Trajectory(ros::Publisher* pub)
@@ -54,32 +56,56 @@ Trajectory::Trajectory(ros::Publisher* pub)
 
 void Trajectory::next_step_lin(const ros::TimerEvent& event)
 {
-    inc++;
-    state.header.stamp = ros::Time::now();
-    pub_ptr->publish(state);
-    state.position[0] += lin_delta_1;
-    state.position[1] += lin_delta_2;
-    state.position[2] += lin_delta_3;
+    if (++inc < msg_amount)
+    {
+        current.position[0] += lin_delta_0;
+        current.position[1] += lin_delta_1;
+        current.position[2] += lin_delta_2;
+    }
+
+    current.header.stamp = ros::Time::now();
+    pub_ptr->publish(current);
 }
 
-void Trajectory::init_lin(double b1, double b2, double b3)
+void Trajectory::set_target(sensor_msgs::JointStateConstPtr new_target)
+{
+    this->target = *new_target;
+}
+
+void Trajectory::init_lin(sensor_msgs::JointStateConstPtr msg)
 {
     inc = 0;
-    this->a1 = state.position[0];
-    this->a2 = state.position[1];
-    this->a3 = state.position[2];
-    this->b1 = b1;
-    this->b2 = b2;
-    this->b3 = b3;
-    lin_delta_1 = (b1-a1)/msg_amount;
-    lin_delta_2 = (b2-a2)/msg_amount;
-    lin_delta_3 = (b3-a3)/msg_amount;
+    initial = current;
+    set_target(msg);
+    lin_delta_0 = (target.position[0]-initial.position[0])/msg_amount;
+    lin_delta_1 = (target.position[1]-initial.position[1])/msg_amount;
+    lin_delta_2 = (target.position[2]-initial.position[2])/msg_amount;
 }
 
 void target_states_cb(const sensor_msgs::JointStateConstPtr &msg, ros::Timer *timer, Trajectory *t)
 {
-    t->init_lin(msg->position[0],msg->position[1],msg->position[2]);
-    timer->start();
+    if (t->compare_target(msg))
+    {
+        t->init_lin(msg);
+        timer->start();
+    }
+    else
+        t->current.header.stamp = ros::Time::now();
+        t->pub_ptr->publish(t->current);
+}
+
+bool Trajectory::compare_target(sensor_msgs::JointStateConstPtr input)
+{
+    double diff = 0, sq;
+    for(int i = 0; i < 3; ++i)
+    {
+        sq = (input->position[i] - target.position[i]);
+        diff += sq*sq;
+    }
+    if (diff > 0.01)
+        return 1;  // a != b
+    else
+        return 0;  // a = b
 }
 
 
