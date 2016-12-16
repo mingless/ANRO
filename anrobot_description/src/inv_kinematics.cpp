c92bd1e25b2ad6b9ab82ed33d66e100f995df177
kosooi <xkubash@gmail.com>
2016-12-16 21:50:28 +0100

    Finished the inv_kinematics node.
    Moved node structure in the cpp file into a class. Fixed the math formula for calculating end effector position.
    Added a few 'add_dependencies' lines in CMake so that service header files get properly found when building.

--- anrobot_description/src/inv_kinematics.cpp
+++ anrobot_description/src/inv_kinematics.cpp
@@ -5,57 +5,61 @@
 #include <math.h>
 
 
-bool get_states(anrobot_description::InvKinematics::Request &req,
-	anrobot_description::InvKinematics::Response &res,
-	ros::Publisher *publisher)
+class States
 {
-	sensor_msgs::JointState state;
-	state.name.push_back("joint1");
-	state.name.push_back("joint2");
-	state.name.push_back("joint3");
-	state.effort.push_back(0);
-	state.effort.push_back(0);
-	state.effort.push_back(0);
-	state.velocity.push_back(0);
-	state.velocity.push_back(0);
-	state.velocity.push_back(0);
-	double x = req.x;
-	double y = req.y;
-	double z = req.z;
-	double a = 2;
-	double b = 1;	
+	private:	
+		ros::NodeHandle n;
+		ros::Publisher inv_kin_pub;
+		ros::ServiceServer inv_kin_client;
+	public:
+		States() {	
+			inv_kin_pub = n.advertise<sensor_msgs::JointState>("calculated_joint_states", 1);
+			inv_kin_client = n.advertiseService("inv_kinematics", &States::get_states, this);
+		}	
+		
+		bool get_states(anrobot_description::InvKinematics::Request &req,
+		anrobot_description::InvKinematics::Response &res)
+		{
+			sensor_msgs::JointState state;
+			state.name.push_back("joint1");
+			state.name.push_back("joint2");
+			state.name.push_back("joint3");
+			state.effort.push_back(0);
+			state.effort.push_back(0);
+			state.effort.push_back(0);
+			state.velocity.push_back(0);
+			state.velocity.push_back(0);
+			state.velocity.push_back(0);
+			double x = req.x;
+			double y = req.y;
+			double z = req.z;
+			double a = 2;
+			double b = 1;	
 
-	double eq = 1 - pow((x*x + y*y - a*a - b*b) / (2*a*b), 2);
-	if(z < -3. || z > -0.3 || eq < 0) {
-		ROS_ERROR_STREAM("Invalid target position\n");
-		return false;
-	}
-	double theta2 = atan2(sqrt(eq),(x*x + y*y - a*a - b*b) / (2*a*b));
-	eq = y*y + x - pow((b*cos(theta2) + a), 2);
-	if(eq < 0) {
-		ROS_ERROR_STREAM("Invalid target position\n");
-		return false;
-	}
-	double theta1 = atan2(sqrt(eq),b*cos(theta2) + a);
-	double d3 = -z;
-	state.position.push_back(theta1);
-	state.position.push_back(theta2);
-	state.position.push_back(d3);
-	publisher->publish(state);
-	res.states = state;
-	return true;
-	
+			double eq = 1 - pow((x*x + y*y - a*a - b*b) / (2*a*b), 2);
+			if(z < -3. || z > -0.3 || eq < 0) {
+				ROS_ERROR_STREAM("Invalid target position\n");
+				return false;
+			}
+			double theta2 = atan2(sqrt(eq),(x*x + y*y - a*a - b*b) / (2*a*b));
+			double theta1 = atan2(y,x) - atan2(b*sin(theta2) ,a + b*cos(theta2));
+			double d3 = -z;
+			state.position.push_back(theta1);
+			state.position.push_back(theta2);
+			state.position.push_back(d3);
+			inv_kin_pub.publish(state);
+			res.states = state;
+			res.success = true;
+			return true;
+				
+		}
+};
 
-	
-}
 
 int main(int argc, char** argv) 
 {
 	ros::init(argc, argv, "inv_kinematics");
-	ros::NodeHandle n;
-	ros::Publisher inv_kin_pub = n.advertise<sensor_msgs::JointState>("calculated_joint_states", 1);
-	ros::ServiceServer inv_kin_client = n.advertiseService("inv_kinematics", boost::bind(get_states, _1, &inv_kin_pub);
-
+	States state;
 
 	ros::spin();
 	return 0;
