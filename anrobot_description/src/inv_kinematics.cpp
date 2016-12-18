908c3341a515373022d76a47ab02b3dabc61e5f0
mingless <mateusz.dziwulski@gmail.com>
2016-12-18 16:46:16 +0100

    Garbage, for reference.

--- anrobot_description/src/inv_kinematics.cpp
+++ anrobot_description/src/inv_kinematics.cpp
@@ -7,16 +7,16 @@
 
 class States
 {
-	private:	
+	private:
 		ros::NodeHandle n;
 		ros::Publisher inv_kin_pub;
 		ros::ServiceServer inv_kin_client;
 	public:
-		States() {	
+		States() {
 			inv_kin_pub = n.advertise<sensor_msgs::JointState>("calculated_joint_states", 1);
 			inv_kin_client = n.advertiseService("inv_kinematics", &States::get_states, this);
-		}	
-		
+		}
+
 		bool get_states(anrobot_description::InvKinematics::Request &req,
 		anrobot_description::InvKinematics::Response &res)
 		{
@@ -34,7 +34,7 @@ class States
 			double y = req.y;
 			double z = req.z;
 			double a = 2;
-			double b = 1;	
+			double b = 1;
 
 			double eq = 1 - pow((x*x + y*y - a*a - b*b) / (2*a*b), 2);
 			if(z < -3. || z > -0.3 || eq < 0) {
@@ -51,12 +51,12 @@ class States
 			res.states = state;
 			res.success = true;
 			return true;
-				
+
 		}
 };
 
 
-int main(int argc, char** argv) 
+int main(int argc, char** argv)
 {
 	ros::init(argc, argv, "inv_kinematics");
 	States state;
