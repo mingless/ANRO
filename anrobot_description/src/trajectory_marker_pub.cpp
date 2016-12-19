da46b02f6139095653fda98ed400d18ae39c0805
Mateusz Dziwulski <mdziwuls@pantera.robotyka.ia.pw.edu.pl>
2016-12-19 11:09:55 +0100

    Added the point_to_marker node.

---
+++ anrobot_description/src/trajectory_marker_pub.cpp
@@ -0,0 +1,49 @@
+#include <ros/ros.h>
+#include <visualization_msgs/Marker.h>
+#include <geometry_msgs/Point.h>
+
+
+
+void get_point_cb(const geometry_msgs::PointConstPtr &msg, Publisher *pub) {
+    
+    visualization_msgs::Marker marker;
+    
+    marker.pose.position.x = msg->x;
+    marker.pose.position.y = msg->y;
+    marker.pose.position.z = msg->z;
+
+    marker.header.frame_id = "/base_link";
+    marker.header.stamp = ros::Time::now();
+
+    marker.ns = "point_trajectory_marker";
+    marker.id = 0;
+    marker.type = visualization_msgs::Marker::SPHERE;
+    marker.action = visualization_msgs::Marker::ADD;
+
+    marker.scale.x = 0.1;
+    marker.scale.y = 0.1;
+    marker.scale.z = 0.1;
+
+    // Set the color -- be sure to set alpha to something non-zero!
+    marker.color.r = 0.8f;
+    marker.color.g = 0.8f;
+    marker.color.b = 0.0f;
+    marker.color.a = 1.0;
+
+    marker.lifetime = ros::Duration();
+    pub->publish(marker);
+}
+
+
+
+
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "point_to_marker");
+    ros::NodeHandle n;
+    ros::Subscriber get_point = n.subscribe<geometry_msgs::Point>("end_trajectory", 100, boost::bind(joint_states_cb, _1, &marker_pub);
+    ros::Publisher marker_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 1);
+    
+    ros::spin()
+
+	return 0;
+}
