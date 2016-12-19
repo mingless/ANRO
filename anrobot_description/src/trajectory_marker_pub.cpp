c134eb82aab157f00457d54a1a9331a9f89f36c2
mingless <mateusz.dziwulski@gmail.com>
2016-12-19 12:39:32 +0100

    Changed marker style.

--- anrobot_description/src/trajectory_marker_pub.cpp
+++ anrobot_description/src/trajectory_marker_pub.cpp
@@ -1,9 +1,9 @@
 #include <ros/ros.h>
-#include <visualization_msgs/Marker.h>
 #include <geometry_msgs/Point.h>
+#include <geometry_msgs/PoseStamped.h>
 
 
-
+/*
 void get_point_cb(const geometry_msgs::PointConstPtr &msg, ros::Publisher *pub) {
     visualization_msgs::Marker marker;
 
@@ -16,7 +16,7 @@ void get_point_cb(const geometry_msgs::PointConstPtr &msg, ros::Publisher *pub) 
 
     marker.ns = "point_trajectory_marker";
     marker.id = 0;
-    marker.type = visualization_msgs::Marker::SPHERE;
+    marker.type = visualization_msgs::Marker::CYLINDER;
     marker.action = visualization_msgs::Marker::ADD;
 
     marker.scale.x = 0.1;
@@ -31,12 +31,25 @@ void get_point_cb(const geometry_msgs::PointConstPtr &msg, ros::Publisher *pub) 
 
     marker.lifetime = ros::Duration();
     pub->publish(marker);
+}*/
+
+void get_point_cb(const geometry_msgs::PointConstPtr &msg, ros::Publisher *pub) {
+    geometry_msgs::PoseStamped pose;
+
+    pose.pose.position.x = msg->x;
+    pose.pose.position.y = msg->y;
+    pose.pose.position.z = msg->z;
+
+    pose.header.frame_id = "/base_link";
+    pose.header.stamp = ros::Time::now();
+
+    pub->publish(pose);
 }
 
 int main(int argc, char **argv) {
     ros::init(argc, argv, "trajectory_marker_pub");
     ros::NodeHandle n;
-    ros::Publisher marker_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 1);
+    ros::Publisher marker_pub = n.advertise<geometry_msgs::PoseStamped>("visualization_marker", 1);
     ros::Subscriber get_point = n.subscribe<geometry_msgs::Point>("end_trajectory", 100, boost::bind(get_point_cb, _1, &marker_pub));
 
     ros::spin();
